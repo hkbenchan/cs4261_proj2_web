4d38e93763b179a719bf3f2e02fe731b32dc1437
Ben Chan <ustbenchan@gmail.com>
2012-11-09 17:40:03 -0500

    a

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -24,6 +24,7 @@ class Welcome extends CI_Controller {
 		//$this->load->library('REST_Controller');
 		//$this->load->view('welcome_message');
 		$this->load->library('curl');
+		echo "loaded";
 		$this->load->helper('fetchinfo');
 		fetch_rotten_tomato(1);
 	}
