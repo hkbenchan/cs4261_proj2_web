43be4e2a1a41f9bb99b8718becff13735947a3bb
Ben Chan <ustbenchan@gmail.com>
2012-11-14 17:54:50 -0500

    try

--- application/controllers/movie.php
+++ application/controllers/movie.php
@@ -14,12 +14,9 @@ class Movie extends REST_Controller {
 		$this->load->helper('file');
 		$data = 'Some file data';
 		$response_a = array();
-		if (! write_file(APPPATH.'movies/i/file.php', $data))
-		{
+		if (! write_file(APPPATH.'movies/i/file.php', $data)) {
 			$response_a['message'] = 'Unable to write the file';
-		}
-		else
-		{
+		} else {
 			$response_a['message'] = 'File written!';
 			$response_a['file_info'] = get_file_info(APPPATH.'movies/i/file.php');
 		}
@@ -34,10 +31,27 @@ class Movie extends REST_Controller {
 		// check if it needs to update
 		
 		$result = fetch_rotten_tomato(1);
+		
+		if (! write_file(APPPATH.'movies/i/box_offices.dat', serialize($result))) {
+			$this->response(array('message'=>'Fail writing file.'), 500);
+		} else {
+			$this->response(array('message'=>'success'),200);
+		}
+		
 		//echo '<pre>'.print_r($result,true).'</pre>';
 		//die();
 		
 		
+	}
+	
+	public function box_offices2_get() {
+		$this->load->helper('file');
+		
+		if ( ! ($content = read_file(APPPATH. 'movies/i/box_offices.dat'))) {
+			$this->response(unserialize($content), 200);
+		} else {
+			$this->response(array('message'=>'Fail to open'), 404);
+		}
 	}
 	
 }
