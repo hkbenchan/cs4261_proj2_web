07aa9f99664b30abdf386360195fc8feb826329b
Ben Chan <ustbenchan@gmail.com>
2012-11-16 02:44:07 -0500

    test

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -62,7 +62,7 @@ class Register extends REST_Controller {
 	}
 	
 	
-	public function memberExists_post()
+	public function member_exists_post()
 	{
 		$this->load->helper(array('form','security'));
 		$this->load->library('form_validation');
